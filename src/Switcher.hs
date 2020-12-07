085f591138daee4c052eb919f9845d3bcda635fc
Lukas <dev.lukas.huwald@gmail.com>
2020-12-07 19:07:37 +0100

    day6

--- src/Switcher.hs
+++ src/Switcher.hs
@@ -5,7 +5,7 @@ import Days.Day2 ( run )
 import Days.Day3 ( run )
 import Days.Day4 ( run )
 import Days.Day5 ( run )
---import Days.Day6
+import Days.Day6 ( run )
 --import Days.Day7
 --import Days.Day8
 --import Days.Day9
@@ -32,7 +32,7 @@ runDay 2 = Days.Day2.run
 runDay 3 = Days.Day3.run 
 runDay 4 = Days.Day4.run
 runDay 5 = Days.Day5.run
---runDay 6 = Days.Day6.run
+runDay 6 = Days.Day6.run
 --runDay 7 = Days.Day7.run
 --runDay 8 = Days.Day8.run
 --runDay 9 = Days.Day9.run
